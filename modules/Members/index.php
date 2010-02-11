f166d3a4438051a8831f75fc4a42598a3c43d0e9
Andy <Andy@localhost>
2010-02-11 01:43:07 +0000

    Updated documentation for the Members module.

--- modules/Members/index.php
+++ modules/Members/index.php
@@ -22,7 +22,7 @@ class Module_Members
 	
     /*
       Function: __construct
-      Begins the stat points distribution page.
+      Begins the memebrs list page.
 	
       If the user is logged in, it saves the database, smarty and player variables as class variables, then displays the page.
 	
