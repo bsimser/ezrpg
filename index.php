52fe64d2a89beae52a9f305bc4ed44e4bd2c736f
Andy <Andy@localhost>
2010-03-13 18:40:01 +0000

    Added a hook for the footer.

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ require_once 'init.php';
 $default_mod = 'Index';
 
 $module_name = ( (isset($_GET['mod']) && ctype_alnum($_GET['mod'])) ? $_GET['mod'] : $default_mod );
-
 $module = ModuleFactory::factory($db, $tpl, $player, $module_name);
-
 $module->start();
+
+//Footer hooks
+$hooks->run_hooks('footer');
 ?>
--- lib/class.hooks.php
+++ lib/class.hooks.php
@@ -110,3 +110,4 @@ class Hooks
         return $ret;
     }
 }
+?>
