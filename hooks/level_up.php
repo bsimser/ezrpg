4c069b4cfbc732f91aee3f37d11efa14a189f052
Andy <Andy@localhost>
2010-03-26 02:06:40 +0000

    Added hp bonus to level up hook.

--- hooks/level_up.php
+++ hooks/level_up.php
@@ -19,9 +19,11 @@ function hook_level_up($db, &$tpl, $player, $args = 0)
         $args->max_exp += 20;
         $args->max_energy += 1;
         $args->energy += 1;
+        $args->hp += 5;
+        $args->max_hp += 5;
         
         //Update the database
-        $db->execute('UPDATE `<ezrpg>players` SET `exp`=?, `level`=level+1, `stat_points`=stat_points+2, `max_exp`=max_exp+20, `energy`=energy+1, `max_energy`=max_energy+1 WHERE `id`=?', array(intval($args->exp), intval($args->id)));
+        $db->execute('UPDATE `<ezrpg>players` SET `exp`=?, `level`=level+1, `stat_points`=stat_points+2, `max_exp`=max_exp+20, `energy`=energy+1, `max_energy`=max_energy+1, `hp`=hp+5, `max_hp`=max_hp+5 WHERE `id`=?', array(intval($args->exp), intval($args->id)));
         
         //Add event log
         $msg = 'You have leveled up! You gained +2 stat points and +1 max energy!';
